bdacbe92506262d9e6433750ac0560b4772b1701
Angel Sanchez <angeloscargonzalezsanchez1993@gmail.com>
2018-07-31 19:53:12 -0500

    modificando color del navegation.css

--- views/modules/navigation.php
+++ views/modules/navigation.php
@@ -13,13 +13,20 @@
 
   }
 
+#mainNav.navbar-dark .navbar-collapse .navbar-sidenav > .nav-item .sidenav-second-level > li > a,
+#mainNav.navbar-dark .navbar-collapse .navbar-sidenav > .nav-item .sidenav-third-level > li > a {
+  color: #0c0c0c!important;
+}
+
+  }
+
 </style>
 
 
 <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
   <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
     <a class="nav-link" href="index.php">
-      <i class="fa fa-fw fa-dashboard"></i>
+      <i class="fa fa-fw fa-dashboard"  style="color: black;"></i>
       <span class="nav-link-text" style="color: black;">Dashboard</span>
     </a>
   </li>
@@ -39,72 +46,72 @@
 
   <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Maestros">
     <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#optMaestros" data-parent="#exampleAccordion">
-      <i class="fa fa-fw fa-child"></i>
+      <i class="fa fa-fw fa-child" style="color:#060606;"></i>
       <span class="nav-link-text" style="color: black;">Maestros</span>
     </a>
     <ul class="sidenav-second-level collapse" id="optMaestros">
       <li>
-        <a href="registrarMaestro" style="color: black;">Registrar maestro</a>
+        <a href="registrarMaestro" >Registrar maestro</a>
+      </li>
+      <li> 
+        <a href="consultarMaestro">Consultar maestro</a>
       </li>
       <li>
-        <a href="consultarMaestro" style="color: black;">Consultar maestro</a>
+        <a href="agregarPuntos">Agregar puntos</a>
       </li>
       <li>
-        <a href="agregarPuntos" style="color: black;">Agregar puntos</a>
-      </li>
-      <li>
-        <a href="reporteMaestros" style="color: black;">Reporte de maestros</a>
+        <a href="reporteMaestros">Reporte de maestros</a>
       </li>
     </ul>
   </li>
 
   <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Ferreterias">
     <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#optFerreterias" data-parent="#exampleAccordion">
-      <i class="fa fa-fw fa-building"></i>
+      <i class="fa fa-fw fa-building"  style="color: #060606"></i>
       <span class="nav-link-text" style="color: black;">Ferreterias</span>
     </a>
     <ul class="sidenav-second-level collapse" id="optFerreterias">
       <li>
-        <a href="registrarFerreteria" style="color: black;">Registrar ferreteria</a>
+        <a href="registrarFerreteria">Registrar ferreteria</a>
       </li>
       <li>
-        <a href="reporteFerreterias" style="color: black;">Reporte ferreterias</a>
+        <a href="reporteFerreterias">Reporte ferreterias</a>
       </li>
     </ul>
   </li>
 
   <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Control">
     <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#optControl" data-parent="#exampleAccordion">
-      <i class="fa fa-fw fa-wrench"></i>
+      <i class="fa fa-fw fa-wrench"  style="color: #060606;"></i>
       <span class="nav-link-text" style="color: black;">Control</span>
     </a>
     <ul class="sidenav-second-level collapse" id="optControl">
       <li>
-        <a href="crearUsuario" style="color: black;">Crear usuario</a>
+        <a href="crearUsuario">Crear usuario</a>
       </li>
       <li>
-        <a href="reporteUsuarios" style="color: black;">Reporte de usuarios</a>
+        <a href="reporteUsuarios">Reporte de usuarios</a>
       </li>
       <li>
-        <a href="validarPuntos" style="color: black;">Validar puntos</a>
+        <a href="validarPuntos">Validar puntos</a>
       </li>
     </ul>
   </li>
 
   <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Reportes">
     <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#optReportes" data-parent="#exampleAccordion">
-      <i class="fa fa-fw fa-file-text"></i>
+      <i class="fa fa-fw fa-file-text"  style="color:#060606;"></i>
       <span class="nav-link-text" style="color: black;">Reportes</span>
     </a>
     <ul class="sidenav-second-level collapse" id="optReportes">
       <li>
-        <a href="reporteHistorial" style="color: black;">Historial de puntos</a>
+        <a href="reporteHistorial">Historial de puntos</a>
       </li>
       <li>
-        <a href="#" style="color: black;">Reporte2</a>
+        <a href="#">Reporte2</a>
       </li>
       <li>
-        <a href="#" style="color: black;">Reporte3</a>
+        <a href="#">Reporte3</a>
       </li>
     </ul>
   </li>
